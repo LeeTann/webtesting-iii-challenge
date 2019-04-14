ca91719be9b2c3720d8e55aa2a67da60890a49e0
ltann <leeroyleeroy18@gmail.com>
2019-04-14 15:38:10 -0700

    Added depenencies and initial test setup

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
 // Test away
+import React from 'react'
+import { render, fireEvent, cleanup } from 'react-testing-library'
+import renderer from 'react-test-renderer'
+
+import Dashboard from './Dashboard'
+
+describe('<Dashboard /> Tests', () => {
+    it('should display component without failing', () => {
+        render(<Dashboard />)
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
 // Test away!
+import React from 'react'
+import { render, fireEvent, cleanup } from 'react-testing-library'
+import renderer from 'react-test-renderer'
+
+import Display from './Display'
+
+describe('<Display /> Tests', () => {
+    it('should display component without failing', () => {
+        render(<Display />)
+    })
+})
